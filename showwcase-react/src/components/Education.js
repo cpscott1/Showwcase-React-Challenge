6e6b2ee005796a6daf11b66876f5a7acacc25d45
cpscott1 <thacam18gmail.com>
2020-09-16 18:38:12 -0500

    returns universities from api

--- showwcase-react/src/components/Education.js
+++ showwcase-react/src/components/Education.js
@@ -1,13 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function Education() {
+  const [education, setEducation] = useState([])
   useEffect(() => {
-    axios.get("http://universities.hipolabs.com/search?")
-    .then(response => console.log(response.data))
+    axios.get("http://universities.hipolabs.com/search?name=Middle")
+    .then(response => {
+      console.log(response.data)
+      setEducation(response.data)
+    })
     .catch(error => console.log(error))
-  })
+  }, [])
   return (
-    <div className="education"></div>
+    <div className="education">
+      {education.map(education => <p>{education.name}</p>)}
+    </div>
   )
 }
