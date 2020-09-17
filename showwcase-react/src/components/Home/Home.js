29ef49554f2154f475b94199e4c30046914b5710
cpscott1 <thacam18gmail.com>
2020-09-16 22:09:53 -0500

    added react router

--- showwcase-react/src/components/Home/Home.js
+++ showwcase-react/src/components/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Button } from 'reactstrap';
+import { useHistory } from 'react-router-dom';
 import './home.css'
 
 export default function Home() {
+  const history = useHistory()
   return (
     <form className="home-form">
       <label htmlFor="yourName">Type your name and click "Enter" below to begin!</label>
@@ -10,7 +12,7 @@ export default function Home() {
         name="yourName"
         placeholder="Your name"
       />
-      <Button className="home-button">Enter</Button>
+      <Button className="home-button" onClick={() => history.push('/main')}>Enter</Button>
     </form>
   )
 }
