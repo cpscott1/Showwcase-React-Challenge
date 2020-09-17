c9389b91571893081e918ddd9941b02ffcb4ea76
cpscott1 <thacam18gmail.com>
2020-09-16 20:34:56 -0500

    added some styling for home screen

--- showwcase-react/src/components/Home/Home.js
+++ showwcase-react/src/components/Home/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Button } from 'reactstrap';
+import './home.css'
 
 export default function Home() {
   return (
-    <form>
+    <form className="home-form">
       <label htmlFor="yourName">Type your name and click "Enter" below to begin!</label>
       <input
         name="yourName"
         placeholder="Your name"
       />
-      <Button>Enter</Button>
+      <Button className="home-button">Enter</Button>
     </form>
   )
 }
