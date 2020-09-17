afbb7136f50d4bea5bf61daf80000fec350ee0f0
cpscott1 <thacam18gmail.com>
2020-09-16 23:09:21 -0500

    add more logic to form

--- showwcase-react/src/components/Home/Home.js
+++ showwcase-react/src/components/Home/Home.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'reactstrap';
 import { useHistory } from 'react-router-dom';
 import './home.css'
 
 export default function Home() {
+  const [name, setName] = useState("")
   const history = useHistory()
   return (
-    <form className="home-form">
+    <form className="home-form" onSubmit={(event) => {
+      event.preventDefault()
+    }}>
       <label htmlFor="yourName">Type your name and click "Enter" below to begin!</label>
       <input
+        id="yourName"
         name="yourName"
         placeholder="Your name"
+        value={name}
+        onChange={(event) => {
+          setName(event.target.value)
+        }}
       />
       <Button className="home-button" onClick={() => history.push('/main')}>Enter</Button>
     </form>
