3401c4eb113f8f69667072bbea22ca96a687efe9
cpscott1 <thacam18gmail.com>
2020-09-16 19:15:33 -0500

    working on form

--- showwcase-react/src/components/Home.js
+++ showwcase-react/src/components/Home.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export default function Home() {
-  
+  return (
+    <form>
+      <input
+      placeholder="Your name"
+      />
+      <button>Enter</button>
+    </form>
+  )
 }
